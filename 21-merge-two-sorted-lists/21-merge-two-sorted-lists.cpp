7546f2456611d91fef42a1d34cf22e8384105d77
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-01-11 16:50:36 +0530

    Time: 8 ms (70.42%), Space: 14.8 MB (83.19%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,54 +11,47 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* mh = NULL;
-        ListNode* h1 = list1;
-        ListNode* h2 = list2;
-        if(list1==NULL && list2==NULL)
+        if(list1==NULL && list2==NULL) return NULL;
+        
+            else if(list1==NULL)
+            {
+                return list2;
+            }
+            else if(list2==NULL) return list1;
+        ListNode* h2 = NULL;
+            ListNode* h1 = NULL;
+        if(list1->val >= list2->val)
         {
-            return NULL;
-        }
-        else if(list2==NULL)return list1;
-        else if(list1==NULL)return list2;
-        if(list1->val >=list2->val)
-        {
-            mh = list2;
-            h2 = h2->next;
+        h2 = list2;
+        h1 = list1;    
         }
         else
         {
-            mh=list1;
-            h1 = h1->next;
+             h2 = list1;
+            h1 = list2;  
         }
-        
         while(h2!=NULL && h1!=NULL)
         {
-            if(h1->val  >=h2->val)
+            ListNode* p2 = NULL;
+            while(h2!=NULL)
             {
-                mh->next = h2;
-                mh = mh->next;
-                h2 = h2->next;
+                if(h1->val >= h2->val)
+                {
+                    p2 = h2;
+                    h2 = h2->next;
+                }
+                else
+                {
+                    break;
+                }
             }
-            else
-            {
-                mh->next = h1;
-                mh = mh->next;
-                h1 = h1->next;
-            }
+                p2->next = h1;
+                ListNode* te = h2;
+                h2 = h1;
+                h1 = te;  
         }
-        while(h2!=NULL)
-        {
-            mh->next = h2;
-            mh = mh->next;
-            h2 = h2->next;
-        }
-        while(h1!=NULL)
-        {
-            mh->next = h1;
-            mh = mh->next;
-            h1 = h1->next;
-        }
-        if(list1->val >=list2->val)return list2;
+        if(list1->val >= list2->val) return list2;
+        
         return list1;
     }
 };
