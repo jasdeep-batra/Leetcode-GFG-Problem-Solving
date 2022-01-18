a3bf76e81ccd15d19ae2590288cddf3eeb574b69
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-01-18 19:47:37 +0530

    Time: 402 ms (5.01%), Space: 91.9 MB (6.81%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     vector<vector<int> > result;
-    void recfun(vector<int> arr,int target, int n, vector<int> row)
+    void recfun(vector<int> arr,int target, int i, vector<int> row)
     {
-        for(int i = n;i>0;i--)
+        if(i==0)
         {
-            cout<<"target: "<<target<<endl;
-            if(arr[i-1]==target)
-            {
-                cout<<"wroking"<<endl;
-                row.push_back(arr[i-1]);
-                result.push_back(row);
-                row.pop_back();
-                continue;
-            }
-            else if((target/arr[i-1])<1)
-            {           
-                cout<<"else if: "<<arr[i-1]<<endl;
-                continue;
-            }                   
-            else
-            {
-                if((target/arr[i-1]) >= 1)
+                if(target==0)
+                {
+                    result.push_back(row);
+                }
+         return;
+        }
+            
+                if(target>=arr[i-1]) 
                 {
                     cout<<"else (if): "<<arr[i-1]<<endl;
                     row.push_back(arr[i-1]);
                     recfun(arr,target-arr[i-1],i,row);
                     row.pop_back();
-                }
-                else{cout<<"else (else): "<<arr[i-1]<<endl;
-                row.push_back(arr[i-1]);
-                recfun(arr,target-arr[i-1],i-1,row);
-                row.pop_back();}
+                }               
+                recfun(arr,target,i-1,row);
+               
                 
-            }
-        }
+            
+        
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
