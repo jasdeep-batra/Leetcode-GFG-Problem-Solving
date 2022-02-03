40868fdeb9c13bce05ec24260fa517dde13236ad
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-02-03 23:53:21 +0530

    Added solution - LeetHub

---
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int equalPartition(int N, int arr[])
+    {
+        int sum =0;
+        for(int i=0;i<N;i++)
+        {
+            sum+=arr[i];
+        }
+        if(sum%2!=0 || N==1)return false;
+        sum = sum/2;
+        vector<vector<int>> dp(N+1,vector<int>(sum+1,false));
+        for(int i=0;i<N+1;i++)
+        {
+            dp[i][0] = true;
+        }
+        for(int i=1;i<sum+1;i++)
+        {
+            dp[0][i] = false;
+        }
+        for(int i=1;i<N+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(j>=arr[i-1])
+                {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];                    
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        // for(int i=0;i<N+1;i++)
+        // {
+        //     for(int j=0;j<sum+1;j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        bool ans = dp[N][sum];
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}  // } Driver Code Ends
