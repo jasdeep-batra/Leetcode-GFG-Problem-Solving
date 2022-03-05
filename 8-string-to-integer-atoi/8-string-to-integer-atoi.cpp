c27ff131f6502cb0412bb6f9a855c88aadbb61db
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-03-05 23:35:11 +0530

    Time: 11 ms (12.40%), Space: 7 MB (54.09%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,65 +1,25 @@
-
 class Solution {
 public:
     int myAtoi(string s) {
-        string new_s;int m=0,n=s.size()-1;
-        long ang=0;
-        for(int i=0;i<s.size();i++)
+        if(s.size()==0)return 0;
+        long ans=0;
+        int ind = 0;
+        int flag = 1;
+        while(ind<s.size() && s[ind]==' ')
         {
-            if(s[i]!=' ')
-            {
-                m = i;
-                break;
-            }
+            ++ind;
         }
-        for(int i=s.size()-1;i>=0;i--)
+        if(ind<s.size() && s[ind]=='-' || s[ind]=='+')
         {
-            if(s[i]!=' ')
-            {
-                n = i;
-                break;
-            }
+            s[ind]=='-'? flag=-1: flag=1;
+            ++ind;
         }
-        new_s = s.substr(m,n-m+1);
-        string ans;int flag = 1;
-        for(int i=0;i<new_s.size();i++)
+        for(int i=ind;i<s.size() && isdigit(s[i]);i++)
         {
-            if((new_s[i]>='a' && new_s[i]<='z') || new_s[i]=='.')
-            {
-                break;
-            }
-            if(new_s[i]=='+' && ans.empty())
-            {
-                ans+=s[i];
-                continue;// flag 1 means negative
-            }
-            if(new_s[i]==' ')break;
-            if(new_s[i]=='-' && ans.empty())
-            {
-                ans+=s[i];
-                flag = -1;
-                continue;// flag 1 means negative
-            }
-            else if((new_s[i]=='-'|| new_s[i]=='+') && !(ans.empty()))
-            {
-                cout<<"works";
-                break;
-            }
-            else {
-                char cc = new_s[i];
-                long dfg = cc-'0';cout<<dfg<<endl;
-                ang = dfg + ang*10;
-                cout<<flag<<endl;
-                ans+=new_s[i];
-                if(flag==1 && ang > INT_MAX)return INT_MAX;
-                if(flag==-1 && -1*ang < INT_MIN)return INT_MIN;
-            }
+            ans = ans*10 + (s[i] -'0');
+            if(flag==-1 && -1*ans < INT_MIN)return INT_MIN;
+            if(flag==1 && ans > INT_MAX)return INT_MAX;
         }
-        if(ans.empty()||ang==0)return 0;
-        if(flag==-1 && (ang*flag) < INT_MIN)return INT_MIN;
-        if(flag==1 && ang*flag > INT_MAX)return INT_MAX;
-        //int anse = stoi(ans);
-        return ang*flag;
+        return int(flag*ans);
     }
 };
-    
