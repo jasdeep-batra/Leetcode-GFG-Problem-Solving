32ef22314451c2c2eaef82f0fe971f0e58160137
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-03-06 17:41:48 +0530

    Time: 16 ms (66.30%), Space: 7.5 MB (25.84%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,12 +1,58 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.size()<needle.size())return -1;
-        int len = needle.size();
-        for(int i=0;i<=haystack.size()-len;i++)
+        //LPS : longest prefix same as suffix;
+        if(needle.empty())return 0;
+        vector<int> lps(needle.size(),0);
+        int i=0,j=1;
+        while(j<needle.size())
         {
-            string check = haystack.substr(i,len);
-            if(check==needle)return i;
+            if(needle[i]==needle[j])
+            {
+                lps[j] = i+1;i++;j++;
+            }
+            else
+            {
+                if(i!=0)
+                {
+                    i = lps[i-1];                    
+                }
+                else
+                {
+                    lps[j] = 0;
+                    j++;
+                }                                
+            }
+        }
+        for(auto itr: lps)
+        {
+            cout<<itr<<endl;
+        }
+        int m = 0;
+        int n = 0;
+        while(m<haystack.size())
+        {
+            if(haystack[m]==needle[n])
+            {
+                m++;
+                n++;
+            }
+            if(n==needle.size())
+            {
+                int ans = m-n;
+                return ans;
+            }
+            if(haystack[m]!=needle[n])
+            {
+                if(n!=0)
+                {
+                    n = lps[n-1];                    
+                }
+                else
+                {
+                    m++;                    
+                }
+            }            
         }
         return -1;
     }
