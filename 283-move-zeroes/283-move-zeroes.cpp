3175b444c62f02d0edec03b71e0b66bbee1450b6
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-03-10 17:19:33 +0530

    Time: 19 ms (93.09%), Space: 19.1 MB (83.86%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        int j=1;
+        while(j<nums.size())
+        {
+            if(nums[i]==0 && nums[j]!=0)
+            {
+                swap(nums[i],nums[j]);
+                i++;j++;
+            }
+            else if(nums[i]==0 && nums[j]==0)j++;
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+    }
+};
