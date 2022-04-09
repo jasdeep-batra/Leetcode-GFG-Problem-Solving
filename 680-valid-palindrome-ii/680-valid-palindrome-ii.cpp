02cf5c75ac005844abfe786ed0fc1d4a3cc97330
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-04-10 00:28:20 +0530

    Time: 99 ms (11.62%), Space: 19.6 MB (69.67%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        int i=0,j=s.size()-1,flag=0;
-        while(i<j)
+        //if the entire string is a palindrome then first and last element should match
+        // so we'll use two pointer approach and we can only remove one element  if we reaches the point where we are gonna remove multiple char then we will stop
+        int i = 0;
+        int j = s.size()-1;
+        while(j>i)
         {
-            if(s[i]!=s[j])
+            if(s[i]!=s[j])            
             {
-                int ni = i+1,nj = j;
-                while(ni<nj && s[ni]==s[nj])
+                int ni = i+1;
+                int nj = j;
+                while(nj>ni && s[ni]==s[nj])
                 {
-                    ni++;nj--;
+                    ni++;
+                    nj--;
                 }
-                int oi = i,oj = j-1;
-                while(oi<oj && s[oi]==s[oj])
+                int oi = i;
+                int oj = j-1;
+                while(oj>oi && s[oi]==s[oj])
                 {
-                    oi++;oj--;
+                    oi++;
+                    oj--;
                 }
                 return ni>=nj || oi>=oj;
             }
-             i++;j--;   
+            i++;
+            j--;
         }
         return true;
     }
