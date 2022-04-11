ae688129fadd80aee87dbe0c3b97e2288ed35b03
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-04-11 19:03:10 +0530

    Time: 32 ms (17.99%), Space: 14 MB (97.37%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,64 +9,17 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root, map<TreeNode*, pair<TreeNode*,int>>& mp)
-    {
-        if(root==NULL)return;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
-        mp[root] = {root,0};
-        while(!q.empty())
-        {
-            int lev = q.front().second;
-            TreeNode* nod = q.front().first;
-            q.pop();
-            if(nod->left)
-            {
-                mp[nod->left] = {nod,lev+1};
-                q.push({nod->left,lev+1});
-            }
-            if(nod->right)
-            {
-                mp[nod->right] = {nod,lev+1};
-                q.push({nod->right,lev+1});
-            }
-        }
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        map<TreeNode*, pair<TreeNode*,int>> mp; // parent,level
-        for(auto itr: mp)
+        if(root==nullptr || root==p || root==q){
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left==NULL || right==NULL)
         {
-            cout<<itr.first<<endl;
+            return (left==nullptr)?right:left;
         }
-        traverse(root,mp);
-        TreeNode* top = p;
-        TreeNode* down = q;
-        if(mp[top].second > mp[down].second){
-            top = q;
-            down = p;
-        }
-        while(top!=root)
-        {
-            int level1 = mp[top].second;
-            int level2 = mp[down].second;
-            TreeNode* parent1 = mp[down].first;
-            if(parent1==top){
-                return top;
-            } 
-            if(parent1==mp[top].first)
-            {
-                return mp[top].first; 
-            }     
-            if(level1==level2)
-            {
-                top = mp[top].first;
-                down = mp[down].first;
-            }
-            else
-            {
-                down = mp[down].first;
-            }
-        }
+        if(left&&right)return root;
         return root;
     }
 };
