6bfc7f2152c7e113174240a7d7d4063d4b87b5fd
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-04-13 20:43:44 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (28.81%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -2,74 +2,43 @@ class Solution {
 public:
     string simplifyPath(string path) {
         stack<string> st;
-        int i = 1;
+        int i = 0;
         string c;
-        while(i<path.size())
+        for(int i=0;i<path.size();i++)
         {
             if(path[i]=='/')
             {
-                if(!c.empty())st.push(c);
-                c.clear();
+                continue;
+            }
+            string c;
+            while(i<path.size() && path[i]!='/')
+            {
+                c+=path[i];
+                i++;
+            }
+            if(c==".")
+            {
+                continue;
+            }
+            else if(c=="..")
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
             }
             else
             {
-                c+=path[i];                
-            }            
-            i++;
+                st.push(c);
+            }
         }
-        //cout<<"wroking";
-        if(!c.empty())st.push(c);
-        stack<string> ss;
-        string answer;
-        // while(!st.empty())
-        // {
-        //     cout<<st.top()<<endl;st.pop();
-        // }
+        if(st.empty())return "/";
+        string ans;
         while(!st.empty())
         {
-            cout<<"stack: "<<st.top()<<endl;
-            if(st.top()=="..")
-            {         
-                if(st.size()==1)
-                {
-                    st.pop();
-                    break;
-                }
-                st.pop();
-                cout<<"if 1"<<endl;
-                stack<string> nst;
-                while(!st.empty() && (st.top()==".."||st.top()=="."))
-                {
-                        if(st.top()=="..")nst.push("..");
-                        st.pop();
-                }
-                cout<<"if 2"<<endl;
-                if(!st.empty())st.pop();
-                else if(st.empty())
-                {
-                    cout<<"empty";
-                    break;
-                }
-                while(!nst.empty()){
-                    st.push(nst.top());
-                    nst.pop();
-                }
-            }
-            else if(st.top()==".")
-            {
-                st.pop();
-            }
-            else{
-                ss.push(st.top());
-                st.pop();
-            }
+            ans = '/' + st.top() + ans;
+            st.pop();
         }
-        if(ss.empty()){cout<<"true"<<endl;return "/";}
-        while(!ss.empty())
-        {
-            answer+='/'+ss.top();ss.pop();
-        }        
-        if(answer[answer.size()-1]=='/')answer.erase(answer.size()-1,1);
-        return answer;
+        return ans;
     }
 };
