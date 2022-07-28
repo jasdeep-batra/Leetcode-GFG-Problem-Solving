82961032418eb304097c45b67ef64fed24bd46c1
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-07-28 18:13:00 +0530

    Time: 4 ms (44.56%), Space: 6.1 MB (10.76%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t num = 1;
+        uint32_t num = 0;
         int i = 0;
-        vector<int> arr(32,0);
+        int pi = 31;
+        //vector<int> arr(32,0);
         while(n)
         {
             int a = n & 1;
-            arr[i++] = a;
+           // arr[i++] = a;
+            num+=pow(2,pi)*a;
+            pi--;
             n = n>>1;
         }
         
-        string ans;
-        uint32_t fans = 0;
-        for(int j=0;j<32;j++)
-        {
-            cout<<j;
-            fans += pow(2,j)*arr[32-j-1];
-        }
-       // cout<<ans;
-        return fans;
+        // string ans;
+        // uint32_t fans = 0;
+        // for(int j=0;j<32;j++)
+        // {
+        //     cout<<j;
+        //     fans += pow(2,j)*arr[32-j-1];
+        // }
+        return num;
     }
 };
