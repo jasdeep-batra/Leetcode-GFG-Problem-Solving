fb39cd5fe58270983bcc741d3537b866bc4d754d
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2022-07-31 09:21:51 +0530

    Time: 65 ms (7.97%), Space: 19.5 MB (91.10%) - LeetHub

---
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        pair<int,int> p;
+        p.first = (abs(nums[0])-0);
+        p.second = nums[0];
+                       
+        for(int i=1;i<nums.size();i++)
+        {
+            int diff = abs(nums[i])-0;
+            if(diff<=p.first)
+            {
+                p.first = diff;
+                p.second = nums[i];
+            }
+        }
+        return p.second;
+    }
+};
