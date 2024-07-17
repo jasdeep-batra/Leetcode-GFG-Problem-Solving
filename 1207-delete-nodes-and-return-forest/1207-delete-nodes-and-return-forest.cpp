85e08b5c0e70126780a52b9f6f3ec75baf1eaf5e
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2024-07-17 22:09:55 +0530

    Time: 23 ms (18.33%), Space: 27.3 MB (21.35%) - LeetHub

---
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<TreeNode*> result;
+    TreeNode* find(TreeNode* &root, int value)
+    {
+        if(root==NULL)return NULL;
+        if(root->val==value)
+        {
+            if(root->left!=NULL) result.push_back(root->left);
+            if(root->right!=NULL)result.push_back(root->right);
+            root=NULL;
+            return NULL;
+        }
+        root->left = find(root->left,value);
+        root->right = find(root->right,value);
+        return root;
+    }
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+            //vector<TreeNode*> result;
+            //result.push_back(root);
+            sort(to_delete.begin(),to_delete.end());
+            for(int i=to_delete.size()-1;i>=0;i--)
+            {
+                find(root,to_delete[i]);
+            }
+            if(root!=NULL) result.push_back(root);
+        return result;
+    }
+};
