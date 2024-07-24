7071ee61f5308320ee255278eef0800741643266
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2024-07-24 22:30:38 +0530

    Time: 38 ms (77.36%), Space: 16.9 MB (49.89%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,23 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+        def count(cit:List[int],val):
+            cnt = 0
+            for i in cit:
+                if i>=val:
+                    cnt+=1
+            return cnt
+
+        j = len(citations)
+        i = 0
+        ans = 0
+        while i<=j:
+            mid = (i+j)//2
+            curr_count = count(citations,mid)
+            if curr_count >= mid:
+                ans = max(ans,mid)
+                i = mid+1
+            else:
+                j = mid-1
+        return ans
+        
