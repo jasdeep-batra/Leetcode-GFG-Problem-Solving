004fdbfef7467e30972a8425741b30f5abe22af4
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2024-08-31 23:21:34 +0530

    Time: 535 ms (80.91%), Space: 28.1 MB (63.6%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -1,35 +1,28 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        max_prob = [0.0]*n
         adl = [[] for _ in range(n)]
         j = 0
         for edge in edges:
-            v = edge[0]
-            u = edge[1]
-            prob = succProb[j]
-            adl[v].append((u,prob))
-            adl[u].append((v,prob))
+            u,v = edge[0],edge[1]
+            adl[u].append((v,succProb[j]))
+            adl[v].append((u,succProb[j]))
             j+=1
+
+        max_prob = [0.0]*n
         max_prob[start_node] = 1.0
-        priority_queue = []
-        heapq.heappush(priority_queue,(-1.0,start_node))
-        while priority_queue:
-            prob,curr = heapq.heappop(priority_queue) #tuple
+        queue = [(-1.0,start_node)]
+        while(queue):
+            prob,curr = heapq.heappop(queue)
             prob = -prob
             if curr==end_node:
                 return prob
             childs = adl[curr]
             for child,cprob in childs:
-                new_prob = prob*cprob
-                if new_prob > max_prob[child]:
-                    max_prob[child] = new_prob
-                    heapq.heappush(priority_queue,(-new_prob,child))
-            
+                curr_prob = prob*cprob
+                if curr_prob > max_prob[child]:
+                    max_prob[child] = curr_prob
+                    heapq.heappush(queue,(-curr_prob,child))
 
         return 0.0
-
-
-
-
 
         
