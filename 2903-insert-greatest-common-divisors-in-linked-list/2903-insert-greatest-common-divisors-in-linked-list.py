da2d7283be3699217219a38f50c5c3ecd7535c6b
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2024-09-10 21:33:23 +0530

    Time: 495 ms (5.09%), Space: 19.7 MB (36.27%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
@@ -9,9 +9,11 @@ class Solution:
         # make num1 always smaller 
         if num1 > num2:
             num1,num2 = num2,num1
-        for i in range(num1+1):
+        ans = 1
+        for i in range(1,num1+1):
             if num1%i==0 and num2%i==0:
-                return i
+                ans = max(ans,i)
+        return ans
         
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
         #math.gcd
@@ -20,7 +22,7 @@ class Solution:
         while ptr2:
             val1 = ptr1.val
             val2 = ptr2.val
-            nval = math.gcd(val1,val2)
+            nval = self.gcd(val1,val2)
             ins = ListNode(nval,ptr2)
             ptr1.next = ins
             ptr1 = ptr2
