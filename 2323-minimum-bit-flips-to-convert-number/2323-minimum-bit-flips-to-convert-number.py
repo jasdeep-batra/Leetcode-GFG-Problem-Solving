aef3fdaa06f9d34d7c9a886df10dabef6a5bc2b8
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2024-09-11 21:53:01 +0530

    Time: 38 ms (37.69%), Space: 16.5 MB (26.96%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -1,26 +1,11 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        bin_aa = bin(start)
-        bin_bb = bin(goal)
-        bin_a = bin_aa[2:]
-        bin_b = bin_bb[2:]
-        # print(bin_a,bin_b)
-        # xor operator means if 1 xor 1 = 0
-        # 0 xor 0 = 0
-        # 1 xor 0  = 1
-        # 0 xor 1 = 1
-        if len(bin_b) > len(bin_a):
-            bin_a,bin_b = bin_b,bin_a
-        if len(bin_a) > len(bin_b):
-            add  = "0"*1
-            bin_b = "0"*(len(bin_a)-len(bin_b)) + bin_b#
-            # print(bin_b)
+        xor = start^goal
         ans = 0
-        # print(bin_a,bin_b)
-        for i in range(len(bin_a)):
-            if bin_b[i]!=bin_a[i]:
+        while xor:
+            if xor & 1: # xor & 1
                 ans+=1
-
+            xor >>= 1
         return ans
 
 
