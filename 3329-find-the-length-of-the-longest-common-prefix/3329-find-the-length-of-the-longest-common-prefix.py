6f5d79fa5a0481d269dbda958a5ef7c2964ced89
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2024-09-24 22:13:42 +0530

    Time: 1181 ms (10.61%), Space: 28.1 MB (94.03%) - LeetHub

---
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        #do we need to use 2d array for each array
+        #how to compare 
+        #KMP Algorithm for finding longeset prefix
+        dictt = set()
+        ans = 0
+        for item in arr1:
+            while item>0:
+                print(item)
+                dictt.add(item)
+                item = item//10
+
+        for item in arr2:
+            while item>0:
+                if item in dictt:
+                    if item > ans:
+                        ans = item
+                        break
+                item = item//10
+        cnt = 0
+        while(ans):
+            cnt+=1
+            ans//=10
+        
+        return cnt
+
+            
+
+
+
+        
