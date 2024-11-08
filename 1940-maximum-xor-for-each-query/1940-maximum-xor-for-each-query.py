1de8e949316ebaa939d9cd09396023eadb3555c5
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2024-11-08 23:44:54 +0530

    Time: 927 ms (5.36%), Space: 32.3 MB (84.59%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
@@ -9,8 +9,8 @@ class Solution:
         for k in range(len(nums)):
             ans = 0
             for i in range(maximumBit):
-                if((cxor&(1<<i))==0):
-                    ans |= (1<<i)
+                if(cxor&(1<<i)==0):
+                    ans = ans+1*pow(2,i)
             res.append(ans)
             cxor = cxor^nums[len(nums)-1-k]
         return res
