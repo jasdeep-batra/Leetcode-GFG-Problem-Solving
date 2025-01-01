b73bcc32e11f22e7d2fa15791836b9cf3f592813
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-01-01 19:04:06 +0530

    Time: 0 ms (100%), Space: 17.7 MB (15.52%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -2,10 +2,17 @@ class Solution:
     def maxScore(self, s: str) -> int:
         # zero on left and one on right
         #formula since it is binary
+        zero = 0
         ans = 0
+        one = s.count('1')
         for i in range(len(s)-1):
-            ans = max(ans,s[0:i+1].count('0')+s[i+1:len(s)+1].count('1'))
-
+            if s[i]=='0':
+                zero+=1
+            else:
+                one-=1
+            ans = max(ans,zero+one)
         return ans
+
+        
 
         
