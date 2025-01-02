75fef69162f7ce6874a806d2ef4144538b7a48c3
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-01-02 21:36:35 +0530

    Time: 23 ms (46.02%), Space: 51.1 MB (9.89%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
@@ -0,0 +1,27 @@
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        result =[]
+        cache = [0]*(len(words)+1)
+
+        vowel = set(['a','e','i','o','u'])
+        ind = 0
+        
+        for k in range(len(words)):
+            if (words[k][0] in vowel) and (words[k][-1] in vowel):
+                cache[k]=1
+        ncache = []*(len(words)+1)        
+        for i in range(1,len(cache)):
+            cache[i] = cache[i]+cache[i-1]
+            # 1 0 1 1 1 0
+            # 1 1 2 3 4 4
+        cache = [0] + cache
+        print(cache)
+        for i,j in queries:
+            result.append(cache[j+1]-cache[i])
+
+        return result
+
+
+
+
+        
