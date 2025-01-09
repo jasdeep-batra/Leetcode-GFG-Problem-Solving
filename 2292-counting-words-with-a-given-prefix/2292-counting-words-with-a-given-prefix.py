07f6bb572e5e4f8fa53eeed0f5549106860621fd
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-01-09 21:03:08 +0530

    Time: 0 ms (100%), Space: 17.8 MB (20.46%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
@@ -2,9 +2,7 @@ class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
         ans = 0
         for item in words:
-            b = len(pref)
-            a = item[:b]
-            if a==pref:
+            if item[:len(pref)]==pref:
                 ans+=1
 
         return ans
