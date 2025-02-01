d97d1d2af9e1ec06bbbf692122422bf39f8d9135
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-02-01 19:33:41 +0530

    Time: 0 ms (100%), Space: 18.8 MB (5.59%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,17 +1,33 @@
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        root = list(range(len(edges) + 1)) # each node is root of own subtree
+        rank = [0]*1001
+        parent = [i for i in range(1001)]
+        def find(node):
+            if parent[node]!=node:
+                parent[node] = find(parent[node])
+            return parent[node]
 
-        def find_root(node):
-            if root[node] != node:
-                root[node] = find_root(root[node]) # set root for node as root of subtree
-            return root[node] # found root of subtree
+        def union(node1,node2):
+            p1 = find(node1)
+            p2 = find(node2)
+            if rank[p1]==rank[p2]:
+                rank[p1]+=1
+            if rank[p1]>rank[p2]:
+                parent[p2] = p1
+                rank[p1]+=1
+            if rank[p2]<rank[p1]:
+                parent[p1] = p2
+                rank[p2]+=1
+        x,y = -1,-1
+        for i,j in edges:
+            root1,root2 = find(i),find(j)
+            if root1==root2:
+                return [i,j]
+            parent[root2] = root1
+        return []
 
-        for node1, node2 in edges: 
-            root1, root2 = find_root(node1), find_root(node2)
+        
 
-            if root1 == root2:
-                return [node1, node2] # both nodes are already in same subtree
- 
-            root[root2] = root1 # found new connection between nodes / subtrees
-            # root[root1] = root2 also works            
+
+        
