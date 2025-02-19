deb28f7f70e549e7fb18964210c81ce52a60ff70
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-02-19 22:40:47 +0530

    Time: 3 ms (74.09%), Space: 11.4 MB (54.98%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -4,22 +4,26 @@ public:
     vector<int> visit;
     vector<char> arr;
 
-    void helper(int n, string s){
+    void helper(int n, string s,int k){
         if (s.size() == n){
             res.push_back(s);
             return;
         }
         for (int i = 0; i < 3; i++){
             if (s.empty() || s.back() != arr[i]){
-                helper(n, s + arr[i]);
+                helper(n, s + arr[i],k);
+                if(res.size()==k){
+                    break;
+                }
             }
         }
+        
     }
 
     string getHappyString(int n, int k) {
         arr = {'a', 'b', 'c'};
         visit.resize(3, 0);
-        helper(n, "");
+        helper(n, "",k);
         return (k <= res.size()) ? res[k - 1] : "";
     }
 };
