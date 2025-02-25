33d61bc3da9e4b24d396dd63775c9a1b23f4fcd1
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-02-25 22:57:07 +0530

    Time: 72 ms (66.94%), Space: 21.6 MB (99.19%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -1,19 +1,23 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
         MOD = 10**9+7
-        dp = [[0,0] for i in range(len(arr))]
-        dp[0] = [1,0]  #odd,even
+        odd_count = 1
+        even_count = 0
+        ans = 0
         if arr[0]%2==0:
-            dp[0] = [0,1]
+            odd_count = 0
+            even_count = 1
+        
         for i in range(1,len(arr)):
-            odd_count = dp[i-1][0]
-            even_count = dp[i-1][1]
-            if arr[i]%2==0:                
-                dp[i] = [odd_count,1+even_count]
+            ans+=(odd_count)
+            if arr[i]%2==0:         
+                even_count = 1+even_count       
             else:
-                dp[i] = [even_count+1,odd_count]
-        print(dp)
-        return sum([dp[i][0]%MOD for i in range(len(dp))])%MOD
+                temp = odd_count
+                odd_count = 1+even_count
+                even_count = temp
+        ans+=odd_count
+        return ans%MOD
 
 
 
