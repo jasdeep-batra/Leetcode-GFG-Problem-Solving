5bcef3e88fcb470e7d7f29902c4b83d0c9e3b73d
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-03-05 21:28:31 +0530

    Time: 0 ms (100%), Space: 17.8 MB (48.12%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,16 +1,3 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        # 1, 5, 13, 
-        # 4,8,12,
-        start = 1
-        const = 4
-        for i in range(1,n):
-            start+=const
-            const+=4
-            # print(start," : ", const)
-
-        return start
-            
-
-
-        
+        return 1 + (n*(n-1))*2
