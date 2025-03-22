b604ea516926e303a28e22168edae43b86e209ed
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-03-22 18:59:33 +0530

    Time: 29 ms (85.15%), Space: 23.4 MB (94.82%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -1,21 +1,20 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n = len(graph)
-        visited = [0] * n  # 0 = unvisited, 1 = visiting, 2 = safe
-        result = []
+        visit = [0]*len(graph)
+        ans = []
+        def traverse(node):
+            if visit[node]:
+                return visit[node]==2
 
-        def dfs(node):
-            if visited[node] != 0:  # Already processed
-                return visited[node] == 2  # Return True if it's safe
-            visited[node] = 1  # Mark as visiting
-            for neighbor in graph[node]:
-                if not dfs(neighbor):  # If any neighbor is not safe
+            visit[node] = 1
+            for nbr in graph[node]:
+                if not traverse(nbr):
                     return False
-            visited[node] = 2  # Mark as safe
+                
+            visit[node] = 2
             return True
-
-        for i in range(n):
-            if dfs(i):  # If the node is safe, add it to the result
-                result.append(i)
         
-        return result
+        for node in range(len(graph)):
+            if traverse(node):
+                ans.append(node)
+        return ans
