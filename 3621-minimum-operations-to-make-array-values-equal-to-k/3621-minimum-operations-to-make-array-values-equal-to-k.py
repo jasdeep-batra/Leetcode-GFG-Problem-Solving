ebab7a3b8b55283d5a931231d52ef23917f7f7c9
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-04-09 23:48:19 +0530

    Time: 68 ms (46.93%), Space: 17.8 MB (53.07%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
@@ -3,13 +3,15 @@ class Solution:
         #if any num smaller then k then return -1
         # if k in nums then ans-1
         # if not then ans
-        dictt = Counter(nums)
+        dictt = [0]*101
+        for index in nums:
+            dictt[index] = 1
         cnt = 0
         flag = False
-        for i,v in dictt.items():
-            if i < k:
+        for i in range(101):
+            if dictt[i]==1 and (i < k):
                 return -1
-            if i>=k:
+            if dictt[i]==1 and i>=k:
                 if i==k:
                     flag = True
                 cnt+=1
