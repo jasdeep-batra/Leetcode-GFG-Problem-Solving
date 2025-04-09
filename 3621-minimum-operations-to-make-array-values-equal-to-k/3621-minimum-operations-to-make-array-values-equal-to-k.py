f488426cc77c899c41c7a20e5bd608df34ded788
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-04-09 23:45:20 +0530

    Time: 64 ms (74.53%), Space: 18 MB (9.43%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,18 +1,22 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        arr = [0]*101
-        for index in nums:
-            arr[index]=1
-
-        st = k+1
-        if k not in nums:
-            st = k
-        
+        #if any num smaller then k then return -1
+        # if k in nums then ans-1
+        # if not then ans
+        dictt = Counter(nums)
         cnt = 0
-        summ = sum(arr) - (1 if k in nums else 0)
-        for i in range(st,len(arr)):
-            if arr[i]>0:
+        flag = False
+        for i,v in dictt.items():
+            if i < k:
+                return -1
+            if i>=k:
+                if i==k:
+                    flag = True
                 cnt+=1
-        return -1 if cnt < summ else cnt
+
+        if flag:
+            return cnt-1
+        return cnt
+                
 
         
