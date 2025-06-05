299038cbb21906641c3899828facd9f50894ce8f
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-06-05 11:38:12 +0530

    Time: 135 ms (63.43%), Space: 20.8 MB (46.64%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,27 +1,25 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m,n = len(mat),len(mat[0])
-        ans = [[float('inf')]*n for _ in range(m)]
-        dirs = ((-1,0),(1,0),(0,-1),(0,1))
+        row,col = len(mat),len(mat[0])
+        visit = [[float('inf')]*col for _ in range(row)]
         queue = deque()
-        for i in range(m):
-            for j in range(n):
+        for i in range(row):
+            for j in range(col):
                 if mat[i][j]==0:
-                    ans[i][j]=0
                     queue.append((i,j))
-
+                    visit[i][j] = 0
+        #start BFS
+        step = 1
         while queue:
-            i,j = queue.popleft()
-            for dx,dy in dirs:
-                x,y = i+dx,j+dy
-                if x>=0 and x<m and y>=0 and y<n:
-                    if ans[x][y] > 1+ans[i][j]:
-                        ans[x][y] = 1 + ans[i][j]
-                        queue.append((x,y))
-
-        return ans
-
-
+            for _ in range(len(queue)):
+                i,j = queue.popleft()
+                for dx,dy in ((0,1),(0,-1),(1,0),(-1,0)):
+                    x,y = i+dx,j+dy
+                    if 0<=x<row and 0<=y<col:
+                        if visit[x][y] > 1+visit[i][j]:
+                            visit[x][y] = visit[i][j]+1
+                            queue.append((x,y))
+        return visit
 
 
         
