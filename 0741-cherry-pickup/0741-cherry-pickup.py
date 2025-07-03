e0f0e71f2de146b3a0b731c4c91de785471511e1
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-07-03 23:48:10 +0530

    Time: 499 ms (41.57%), Space: 60.1 MB (26.24%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.py
+++ 0741-cherry-pickup/0741-cherry-pickup.py
@@ -2,15 +2,15 @@ class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         m,n = len(grid),len(grid[0])
         maxcherries = [0]
-        dp = [[[None for _ in range(n)] for _ in range(n)] for _ in range(m)]
+        memo = {}
         def recursion(i,j,k):
             #base condition
             l = i+j-k
             if i>=m or j>=n or k>=m or l>=n or grid[i][j]==-1 or grid[k][l]==-1:                
                 return -sys.maxsize      
             #base condition
-            if dp[i][j][k]!=None:
-                return dp[i][j][k]
+            if (i,j,k) in memo:
+                return memo[(i,j,k)]
             if i==m-1 and j==n-1 and k==m-1 and l==n-1:
                 return grid[i][j]
             if i==k and j==l:
@@ -23,7 +23,7 @@ class Solution:
             ans3 = recursion(i+1,j,k)
             ans4 = recursion(i,j+1,k)
             cherry += max(ans1,ans2,ans3,ans4)
-            dp[i][j][k] = cherry
+            memo[(i,j,k)] = cherry
             return cherry
         
         return max(recursion(0,0,0),0)
