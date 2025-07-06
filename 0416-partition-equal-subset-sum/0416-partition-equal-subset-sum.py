fe5627f9d54e9faab475c35b946ede3b704d2eb8
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-07-06 22:58:58 +0530

    Time: 1663 ms (26.39%), Space: 56.7 MB (22.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,20 +1,20 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        dictt = {}
         summ = sum(nums)
         if summ%2!=0:
             return False
+        memo = [[-1 for i in range(summ//2+1)] for i in range(len(nums))]
         def recursion(i,target):
             if target==0:
                 return True
             if i>=len(nums):
                 return False
             
-            if (i,target) in dictt:
-                return dictt[(i,target)]
+            if memo[i][target]!=-1:
+                return memo[i][target]
             
             found = recursion(i+1,target-nums[i]) or recursion(i+1,target)
-            dictt[(i,target)] = found
+            memo[i][target] = found
             return found
         
         return recursion(0,summ//2)
