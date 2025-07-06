2df2bee160d0d008d6f0c69f752c93964718da4a
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-07-06 19:25:06 +0530

    Time: 51 ms (64.49%), Space: 27.4 MB (5.07%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -5,13 +5,14 @@ class Solution:
         mat = [[[-1 for i in range(maxMove+1) ] for i in range(n)]for i in range(m)]
         MOD = 10**9+7
         dp =  {}
+        @lru_cache(None)
         def recursion(i,j,moves):
             if i<0 or j<0 or i>=m or j>=n:
                 return 1
             if moves==0:
                 return 0
-            if mat[i][j][moves]!=-1:
-                return mat[i][j][moves]%MOD
+            # if mat[i][j][moves]!=-1:
+            #     return mat[i][j][moves]%MOD
             path = recursion(i+1,j,moves-1)+recursion(i,j+1,moves-1)+recursion(i-1,j,moves-1)+recursion(i,j-1,moves-1)
             mat[i][j][moves] = path%MOD
             
