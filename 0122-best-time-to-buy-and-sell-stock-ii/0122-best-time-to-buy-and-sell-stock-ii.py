4c735db4542b78524c2ad64c54459d3a9e5a5a89
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-07-21 22:17:43 +0530

    Time: 12 ms (8.43%), Space: 21.2 MB (7.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,16 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # at every step we have two options either buy or sell there if you sell then buy at next or if you buy then sell at next r somewhere
-        #should we use recursion
-        profit = [0]*(len(prices)+1)
-        mini = [0]*(len(prices)+1)
-        mini[0] = prices[0]
-        #could be a reverse dp
-        for i in range(1,len(prices)):
-            # mini[i] = min(mini[i-1],prices[i])
-            profit[i] = max(profit[i],profit[i-1]+prices[i]-prices[i-1],profit[i-1])
+        dp = [[0]*2 for i in range(len(prices)+1)]
+        for i in range(len(prices)-1,-1,-1):
+            dp[i][0] = max(prices[i]+dp[i+1][1],dp[i+1][0])
+            dp[i][1] = max(-prices[i]+dp[i+1][0],dp[i+1][1])
 
-        return max(profit)
-
-
-
+        return dp[0][1]
