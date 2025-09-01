ebe9ead5c2eded4c337a560e289aecf940de0c77
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-09-01 20:10:40 +0530

    Time: 44 ms (80.48%), Space: 19.4 MB (89.26%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,23 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        #there for we have three choices
-        m = len(word1)
-        n = len(word2)
-        dp = [[0 for _ in range(n+1)] for _ in range( m+1)]
-        for i in range(m+1):
-            dp[i][0] = i
-        for i in range(1,n+1):
-            dp[0][i] = i
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                if word1[i-1]==word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-
-                else:
-                    dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])
-
-        return dp[m][n]
-
-
-
-        
+        memo = {}
+        def helper(i,j):
+            #some base condition
+            if j>=len(word2):
+                return len(word1)-i
+            if i>=len(word1):
+                return len(word2)-j
+            if (i,j) in memo:
+                return memo[(i,j)]
+            op = 0
+            if word1[i]==word2[j]:
+                op = helper(i+1,j+1)
+            else:
+                op = 1 + min(helper(i+1,j+1),helper(i+1,j),helper(i,j+1))
+            memo[(i,j)] = op
+            return op
+        return helper(0,0)
