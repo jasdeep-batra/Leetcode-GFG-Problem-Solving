a45decf047c575a1fd889e95c0b3fb8ce2eb2992
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2025-09-07 11:41:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,19 +1,16 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        #base condition
         @lru_cache(None)
         def helper(i,j):
-            #base condition
-            if j>=len(p):
-                return i==len(s)
-            
-            curr_char = i<len(s) and (s[i]==p[j] or p[j]=='.')
+            if j==len(p):
+                return i>=len(s)
 
-            if j+1<len(p) and p[j+1]=='*':
-                return helper(i,j+2) or (curr_char and helper(i+1,j))
+            if i<len(s):
+                firstchar = s[i]==p[j] or p[j]=='.'
+            if j+1<len(p)  and p[j+1]=='*':
+                ans = helper(i,j+2) or helper(i+1,j)
             else:
-                return curr_char and helper(i+1,j+1)
+                ans = firstchar and helper(i+1,j+1)
+            return ans
         return helper(0,0)
-
-
-
-        
