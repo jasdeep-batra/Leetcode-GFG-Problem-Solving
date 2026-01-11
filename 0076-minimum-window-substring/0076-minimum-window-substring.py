c565da94387b220bd9f6eefd3af26e9e5d6300f6
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2026-01-11 19:48:39 +0530

    Time: 562 ms (8.58%), Space: 19.8 MB (6.44%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,38 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        mapp = {}
-        for item in t:
-            if item not in mapp:
-                mapp[item] = 1
-            else:
-                mapp[item]+=1
+        freq = defaultdict(int)
+        for char in t:
+            freq[char]+=1
         
         i,j = 0,0
-        count = len(t)
-        min_length = float('inf')
-        ans = ""
-        start = 0
-        while(j<len(s)):
-            if s[j] in mapp:
-                mapp[s[j]] -= 1
-                if mapp[s[j]]>=0:
-                    count-=1
-                
-            while count==0:
-                if j-i+1 < min_length:
-                    min_length = j-i+1
-                    start = i
-                if s[i] in mapp:
-                    mapp[s[i]]+=1   
-                    if mapp[s[i]]>0:                        
-                        count+=1
-                i+=1
-            j+=1 
-        return s[start:start + min_length] if min_length != float('inf') else ""
-
-
+        ans = float('inf')
+        res = ""
+        def get_sum_of_freq():
+            return sum([val for char, val in freq.items() if val > 0])
+        while j < len(s):
+            if s[j] in freq:
+                freq[s[j]]-=1
             
+            while get_sum_of_freq()==0:
+                if (j - i + 1) < ans:
+                    ans = j - i + 1
+                    res = s[i:j+1]
+                
+                if s[i] in freq:
+                    freq[s[i]]+=1
+                i+=1
 
+            j+=1
 
-
-        
+        return res
