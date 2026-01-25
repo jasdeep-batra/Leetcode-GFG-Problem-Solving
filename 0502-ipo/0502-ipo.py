ce0e57e7224eaf5e3f2499ae3098c0366a94a197
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2026-01-25 19:34:46 +0530

    Time: 270 ms (73.27%), Space: 48.2 MB (7.34%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,24 +1,17 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        #edge case  
-        heap1 = [(capital[i],profits[i]) for i in range(len(profits))]
-        heapq.heapify(heap1)#think about storing index
-        ans = 0
-        heap2 = []
-        cc=  w
-        while k:
-            while heap1 and heap1[0][0] <= cc:
-                cap,profit = heapq.heappop(heap1)
-                heapq.heappush(heap2,-profit)
-
-            if not heap2:
+        sort_arr = sorted([(capital[i],profits[i]) for i in range(len(profits))])
+        pq = []
+        j = 0
+        for i in range(k):
+            while j<len(sort_arr) and sort_arr[j][0]<=w:
+                cap,prof = sort_arr[j]
+                heapq.heappush(pq,(-prof))
+                j+=1
+            
+            if not pq:
                 break
-            k-=1
-            maxone = heapq.heappop(heap2)
-            cc+=(-maxone)
-        return cc
+            w+=-heapq.heappop(pq)
 
-
-
-
+        return w
 
