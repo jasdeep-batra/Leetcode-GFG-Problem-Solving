204bd3d348cd5a3b542abba682d8fd4a5aa0ce32
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2026-02-04 22:52:39 +0530

    Time: 114 ms (86.3%), Space: 33.7 MB (32.75%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -3,23 +3,12 @@ class Solution:
         n = len(heights)
         left,right = [-1]*n,[n]*n
         stack = []
-        for i in range(n):
-            if not stack:
-                stack.append(i)
-            while stack and heights[stack[-1]] > heights[i]:
-                right[stack[-1]] = i
-                stack.pop()
-            stack.append(i)
-        stack = []
-        for i in range(len(heights)-1,-1,-1):
-            if not stack:
-                stack.append(i)
-            while stack and heights[stack[-1]] > heights[i]:
-                left[stack[-1]] = i
-                stack.pop()
-            stack.append(i)
         area = 0
-        for i in range(n):
-            area = max(area,heights[i]*(right[i]-left[i]-1))
-
+        heights.append(0)
+        for i in range(n+1):
+            while stack and heights[stack[-1]] > heights[i]:
+                height = heights[stack.pop()]
+                width = i if not stack else i-stack[-1] -1 
+                area = max(area,height*width)
+            stack.append(i)
         return area
