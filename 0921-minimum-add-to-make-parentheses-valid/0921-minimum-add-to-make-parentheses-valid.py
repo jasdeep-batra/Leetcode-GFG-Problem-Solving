552077738d079271afe4e0ba7e877328ec6e9c5e
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2026-02-07 02:29:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,27 +1,16 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        #should we determine LVS then substract
-        o,c = 0,0
-        ans = 0
-        for i in s:
-            if i=='(':
-                o+=1
-            if i==')':
-                c+=1
-            if o==c:
-                ans +=2*o
-            elif c>o:
-                o,c=0,0
-        print(ans)
-        o,c = 0,0
-        for i in reversed(s):
-            if i==')':
-                c+=1
-            if i=='(':
-                o+=1
-            if o==c:
-                ans+= 2*c
-            elif o>c:
-                o,c=0,0
-        print(ans)
-        return len(s)-ans
+        stack = []
+        for item in s:
+            if not stack:
+                stack.append(item)
+            if item=='(':
+                stack.append(item)
+            
+            if item==')':
+                if stack[-1]=='(':
+                    stack.pop() 
+                else:
+                    stack.append(item)
+
+        return len(stack)
