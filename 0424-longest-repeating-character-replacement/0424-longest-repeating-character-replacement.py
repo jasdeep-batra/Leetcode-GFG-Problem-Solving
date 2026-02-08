c3feadff9818e395340896d45247cf4317592bb5
Jasdeep Batra <86816357+jasdeep-batra@users.noreply.github.com>
2026-02-09 00:29:46 +0530

    Time: 79 ms (85.79%), Space: 19.7 MB (50.22%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,19 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        dictt = Counter()
-        max_freq = 0
-        i,j =0,0
-        ans = 0
-        while j<len(s):
-            dictt[s[j]]+=1
-            max_freq = max(max_freq,dictt[s[j]])
+        #global vs local problem here we dont have a fix char which has max freq so it can change wqith window
+        #there form no fixed char
+        count  = {}
 
-            while ((j-i+1) - max_freq) > k:
-                dictt[s[i]]-=1
+        i,j = 0,0
+        max_freq = 0
+        ans = 0
+        for j in range(len(s)):
+            count[s[j]] = count.get(s[j],0)+1
+            max_freq = max(max_freq,count[s[j]])
+
+            while (j-i+1) - max_freq > k:
+                count[s[i]]-=1
                 i+=1
-            
+
             ans = max(ans,j-i+1)
-            j+=1
-                
 
         return ans
